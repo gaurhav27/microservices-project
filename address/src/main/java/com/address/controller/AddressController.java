5922ee1f4bbebfa05f100787b3d7863faed19a2a
gaurhav27 <gaurhav.fido@gmail.com>
2026-01-24 13:09:01 +0530

    AddressController modified

--- address/src/main/java/com/address/controller/AddressController.java
+++ address/src/main/java/com/address/controller/AddressController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("addresses")
+@RequestMapping("/addresses")
 public class AddressController {
 
     private final AddressService addressService;
