118c6623508fb830e9683b28d13eeccc1c18a406
gaurhav27 <gaurhav.fido@gmail.com>
2026-01-29 18:09:56 +0530

    Add API Gateway service and integrate employee routing

---
+++ api-gateway/src/main/java/com/apiGateway/filter/AuthFilter.java
@@ -0,0 +1,52 @@
+package com.apiGateway.filter;
+
+import com.apiGateway.exception.BadRequestException;
+import org.apache.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private Validator validator;
+
+    public AuthFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            if (validator.predicate.test(exchange.getRequest())) {
+
+                if(!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    throw new BadRequestException("Authorization header is missing", HttpStatus.UNAUTHORIZED);
+                }
+
+                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+                String token = null;
+                if(null != authHeader && authHeader.startsWith("Bearer ")) {
+                    token = authHeader.substring(7);
+                }
+                try {
+                    jwtUtil.validateToken(token);
+                } catch (Exception e) {
+                    throw new BadRequestException("Invalid token", HttpStatus.UNAUTHORIZED);
+                }
+            }
+            return chain.filter(exchange);
+        };
+
+    }
+
+    public static class Config {
+
+    }
+}
