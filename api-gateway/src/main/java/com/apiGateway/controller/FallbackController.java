c6ec9d3dbf0bd937f444e54b2ed7f251eb65064e
gaurhav27 <gaurhav.fido@gmail.com>
2026-02-04 19:32:09 +0530

    Adding Circuit-Breaker

---
+++ api-gateway/src/main/java/com/apiGateway/controller/FallbackController.java
@@ -0,0 +1,21 @@
+package com.apiGateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/employeeServiceFallback")
+    public Mono<String> employeeFallbackMethod() {
+        return Mono.just("Employee Service is Down. Please try again later.");
+    }
+
+    @GetMapping("/addressServiceFallback")
+    public Mono<String> addressFallbackMethod() {
+        return Mono.just("Address Service is Down. Please try again later.");
+    }
+
+
+}
